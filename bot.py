179f8fb411b9eed4390ebca2ab381a204f906edb
oepop <oepop@gmail.com>
2022-12-07 08:04:18 +0800

    use schedule

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import time
+import schedule
 from datetime import date, datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -14,10 +15,6 @@ driver.get('https://zh.surveymonkey.com/r/EmployeeHealthCheck')
 # height = driver.execute_script("return document.documentElement.scrollHeight")
 # driver.set_window_size(width, height)
 
-# date
-today = date.today()
-mdy = today.strftime("%m/%d/%Y")
-print(mdy)
 # get field that need to fill
 q0 = driver.find_element_by_xpath('//*[@id="97301341_751278109_label"]/span[1]')
 q1 = driver.find_element_by_xpath('//*[@id="97301339"]')
@@ -26,20 +23,31 @@ q3 = driver.find_element_by_xpath('//*[@id="97301347_751278143_label"]/span[1]')
 q4 = driver.find_element_by_xpath('//*[@id="97301348_751278146_DMY"]')
 q5 = driver.find_element_by_xpath('//*[@id="97301340_751278108_label"]/span[1]')
 submit = driver.find_element_by_xpath('//*[@id="patas"]/main/article/section/form/div[2]/button')
-# fill
-q0.click()
-q1.send_keys('') # Employee Id
-q2.click()
-q3.click()
-q4.send_keys(mdy)
-q5.click()
-submit.click()
 
-time.sleep(3)
+def fill():
+    # date
+    today = date.today()
+    mdy = today.strftime("%m/%d/%Y")
+    print(mdy)
+    # fill
+    q0.click()
+    q1.send_keys('') # Employee Id
+    q2.click()
+    q3.click()
+    q4.send_keys(mdy)
+    q5.click()
+    submit.click()
+    # write log
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    logfile = open("logfile.txt", "a")
+    logfile.write(mdy + " " + current_time + " : fill a temperture form\n")
+    logfile.close()
+
+schedule.every().day.at("07:30").do(fill)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
 driver.close()
-
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-logfile = open("logfile.txt", "a")
-logfile.write(mdy + " " + current_time + " : fill a temperture form\n")
-logfile.close()
