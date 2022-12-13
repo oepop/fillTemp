a22e9596c3967f603fc3404966b28a3a5b369b2f
oepop <oepop@gmail.com>
2022-12-13 08:06:35 +0800

    work

--- bot.py
+++ bot.py
@@ -9,22 +9,17 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome('./chromedriver/stable/chromedriver',chrome_options=chrome_options)
-driver.get('https://zh.surveymonkey.com/r/EmployeeHealthCheck')
-
-# width = driver.execute_script("return document.documentElement.scrollWidth")
-# height = driver.execute_script("return document.documentElement.scrollHeight")
-# driver.set_window_size(width, height)
-
-# get field that need to fill
-q0 = driver.find_element_by_xpath('//*[@id="97301341_751278109_label"]/span[1]')
-q1 = driver.find_element_by_xpath('//*[@id="97301339"]')
-q2 = driver.find_element_by_xpath('//*[@id="97301346_751278156_label"]/span[1]')
-q3 = driver.find_element_by_xpath('//*[@id="97301347_751278143_label"]/span[1]')
-q4 = driver.find_element_by_xpath('//*[@id="97301348_751278146_DMY"]')
-q5 = driver.find_element_by_xpath('//*[@id="97301340_751278108_label"]/span[1]')
-submit = driver.find_element_by_xpath('//*[@id="patas"]/main/article/section/form/div[2]/button')
 
 def fill():
+    driver.get('https://zh.surveymonkey.com/r/EmployeeHealthCheck')
+    # get field that need to fill
+    q0 = driver.find_element_by_xpath('//*[@id="97301341_751278109_label"]/span[1]')
+    q1 = driver.find_element_by_xpath('//*[@id="97301339"]')
+    q2 = driver.find_element_by_xpath('//*[@id="97301346_751278156_label"]/span[1]')
+    q3 = driver.find_element_by_xpath('//*[@id="97301347_751278143_label"]/span[1]')
+    q4 = driver.find_element_by_xpath('//*[@id="97301348_751278146_DMY"]')
+    q5 = driver.find_element_by_xpath('//*[@id="97301340_751278108_label"]/span[1]')
+    submit = driver.find_element_by_xpath('//*[@id="patas"]/main/article/section/form/div[2]/button')
     # date
     today = date.today()
     mdy = today.strftime("%m/%d/%Y")
@@ -44,10 +39,13 @@ def fill():
     logfile.write(mdy + " " + current_time + " : fill a temperture form\n")
     logfile.close()
 
+
 schedule.every().day.at("07:30").do(fill)
 
 while True:
     schedule.run_pending()
     time.sleep(1)
+
+# fill()
 
 driver.close()
